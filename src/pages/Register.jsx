fab93a9ebc9a58d4ee1002d763098b94ac305e04
Roldan Bacilio Puon <roldan.bacilio@hotmail.com>
2024-02-29 19:34:40 -0600

    Antes de completar Redux

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -19,6 +19,11 @@ const Register = () => {
         }))
     }
 
+    const onSubmit = (e) =>{
+        e.preventDefault()
+    }
+
+
     return (
         <>
             <section className='heading'>
@@ -27,7 +32,7 @@ const Register = () => {
             </section>
 
             <section className="form">
-                <form>
+                <form onSubmit={onSubmit}>
                     <div className="form-group">
                         <input
                             type="text"
@@ -38,6 +43,44 @@ const Register = () => {
                             placeholder='Por favor escribe tu nombre'
                             onChange={onChange}
                         />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="email"
+                            className='form-control'
+                            id='email'
+                            name='email'
+                            value={email}
+                            placeholder='Por favor escribe tu email'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className='form-control'
+                            id='password'
+                            name='password'
+                            value={password}
+                            placeholder='Por favor escribe tu password'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className='form-control'
+                            id='password2'
+                            name='password2'
+                            value={password2}
+                            placeholder='Por favor confirma tu  password'
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button type='submit' className='btn btn-block'>
+                            crear
+                        </button>
                     </div>
                 </form>
             </section>
